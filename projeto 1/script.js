e0e4304cc3b57b81350cdbbddf3b4f568ec15137
Ricardo Gimenez <cpd2@santanna.g12.br>
2024-01-23 09:31:19 -0300

    0:35:27 - projeto 1 quase finalizado

--- projeto 1/script.js
+++ projeto 1/script.js
@@ -1,16 +1,63 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-
 const CANVAS_WIDTH = (canvas.width = 600);
 const CANVAS_HEIGHT = (canvas.height = 600);
 
 const playerImage = new Image();
 playerImage.src = 'shadow_dog.png';
+const spriteWidth = 575;
+const spriteHeight = 523;
+let frameX = 0;
+let frameY = 0;
+let gameFrame = 0;
+
+// a cada 5 frames a animação passa para a próxima imagem
+const staggerFrames = 5;
+
+const spriteAnimations = [];
+
+const animationStates = [
+  {
+    name: 'idle',
+    frames: 7,
+  },
+  {
+    name: 'jump',
+    frames: 7,
+  },
+];
+animationStates.forEach((state, index) => {
+  let frames = {
+    loc: [],
+  };
+  for (let j = 0; j < state.frames; j++) {
+    let positionX = j * spriteWidth;
+    let positionY = index * spriteHeight;
+    frames.loc.push({ x: positionX, y: positionY });
+  }
+  spriteAnimations[state.name] = frames;
+});
+console.log(spriteAnimations);
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  ctx.fillRect(50, 50, 100, 100);
 
+  let position = Math.floor(gameFrame / staggerFrames) % 6;
+  frameX = position * spriteWidth;
+
+  ctx.drawImage(
+    playerImage,
+    frameX,
+    frameY * spriteHeight,
+    spriteWidth,
+    spriteHeight,
+    0,
+    0,
+    spriteWidth,
+    spriteHeight
+  );
+
+  gameFrame++;
   requestAnimationFrame(animate);
 }
 
